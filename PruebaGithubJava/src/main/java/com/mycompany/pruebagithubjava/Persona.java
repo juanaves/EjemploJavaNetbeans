4df4aa1347f982095c113834f700f6d7c235470d
juan <juan@LAPTOP-PH0SBLLQ>
2022-09-28 09:09:38 +0200

    nuestro primer commit

---
+++ PruebaGithubJava/src/main/java/com/mycompany/pruebagithubjava/Persona.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebagithubjava;
+
+/**
+ *
+ * @author juan
+ */
+public class Persona {
+    int edad;
+
+    public Persona(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
